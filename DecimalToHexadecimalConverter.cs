9dad7d3863b4f71722d3ffc70c13203bc1bc6b87
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2023-06-04 13:40:20 +0200

    Fixed a bug where the first number was not calculated
    - added comments
    - code refactoring
    - error handling
    - fixed bug where first number was not calculated

--- DecimalToHexadecimalConverter.cs
+++ DecimalToHexadecimalConverter.cs
@@ -1,72 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DecimalHexadecimalConverter
 {
-    public static class DecimalToHexadecimalConverter 
+    public static class DecimalToHexadecimalConverter
     {
         public static double EnteredNumber { get; set; }
+
+        /// <summary>
+        /// The last number we divided by 16 (rounded off).
+        /// </summary>
         public static double OldCalculationNumber { get; set; }
+
+        /// <summary>
+        /// The next number divided by 16.
+        /// </summary>
         public static double CurrentCalculationNumber { get; set; }
+
+
+        /// <summary>
+        /// The remainder of the most recent ConvertNumber() calculation.
+        /// </summary>
+        public static double Remainder { get; set; } = 0;
+
+
+        /// <summary>
+        /// The calculation is finished once the current calculation number is smaller than 1.
+        /// </summary>
+        public static bool CalculationFinished { get; set; } = false;
+
+
+        /// <summary>
+        /// All division remainders are added to this list, reversed and displayed as the converted number.
+        /// </summary>
         public static List<object> DivisionRemainders { get; set; } = new List<object>();
 
+
         static void Main(string[] args)
-        {            
-            ConvertNumber(EnteredNumber);
+        {
+            EnteredNumber = GetNumber();
+            ConvertNumber();
             AssembleConvertedNumber();
         }
 
-        public static void ConvertNumber(double oldNumber)
-        {
-            CurrentCalculationNumber = GetNumber();
-            double remainder = 0;
 
-            while (!(CurrentCalculationNumber < remainder))
+        /// <summary>
+        /// Converts the entered number
+        /// </summary>
+        public static void ConvertNumber()
+        {
+
+            // The first calculation is done on the number entered by the user.
+            CurrentCalculationNumber = EnteredNumber;
+            Remainder = 0;
+
+            while (!CalculationFinished)
             {
+
                 OldCalculationNumber = Math.Floor(CurrentCalculationNumber);
                 CurrentCalculationNumber = OldCalculationNumber / 16;
-                remainder = OldCalculationNumber - Math.Floor(CurrentCalculationNumber) * 16;
 
+                // Remainder = difference between the old and the new number we divided by 16.
+                Remainder = OldCalculationNumber - Math.Floor(CurrentCalculationNumber) * 16;
+
+                // If our new number is smaller than 1, the calculation finished.
                 if (CurrentCalculationNumber < 1)
                 {
-                    remainder = Math.Floor(OldCalculationNumber);
+                    CalculationFinished = true;
+                    AddNumberOrLetter(Math.Ceiling(CurrentCalculationNumber));
+                    return;
                 }
 
-                AddNumberOrLetter(remainder);
-
+                // Our new number is greater than zero, the calculation continues.
+                AddNumberOrLetter(Remainder);
             }
+
         }
 
+
+        /// <summary>
+        /// Asks the user to enter a number and saves it in the "EnteredNumber" property.
+        /// </summary>
+        /// <returns></returns>
         public static double GetNumber()
         {
-            Console.Write("Which number would you like to convert? (positive integer): ");
-            EnteredNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Decimal To Hexadecimal Converter ---------------------------------");
+            Console.Write("What number would you like to convert? (must be a positive integer): ");
 
-            while (EnteredNumber < 0 || EnteredNumber % 1 != 0)
+            try
             {
-                Console.WriteLine("Number must be a positive integer)");
-                EnteredNumber = Convert.ToInt32(Console.ReadLine());
+                EnteredNumber = Convert.ToInt64(Console.ReadLine());
+
+                while (EnteredNumber < 0 || EnteredNumber % 1 != 0)
+                {
+                    Console.WriteLine("Number must be a positive integer)");
+                    EnteredNumber = Convert.ToInt64(Console.ReadLine());
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
             }
 
             return EnteredNumber;
         }
 
 
+        /// <summary>
+        /// Takes the remainder of ConvertNumber() and transforms it to its hexadecimal equivalent. 
+        /// </summary>
+        /// <param name="remainder"></param>
         public static void AddNumberOrLetter(double remainder)
         {
-            if (remainder < 9)
+            if (Remainder <= 9)
             {
-                DivisionRemainders.Add(remainder);
+                DivisionRemainders.Add(Remainder);
                 return;
             }
 
             string convertedLetter = null;
 
-            switch (remainder)
+            switch (Remainder)
             {
                 case 10:
                     convertedLetter = "A";
@@ -100,6 +154,10 @@ namespace DecimalHexadecimalConverter
             DivisionRemainders.Add(convertedLetter);
         }
 
+
+        /// <summary>
+        /// Takes all division remainders, reverses and displays them on the console.
+        /// </summary>
         public static void AssembleConvertedNumber()
         {
             DivisionRemainders.Reverse();
